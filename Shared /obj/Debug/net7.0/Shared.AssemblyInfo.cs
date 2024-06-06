180199c5cfd515e5f3c7d3f0296ee92f69ca3a35
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-06-05 23:18:19 -0500

    fix: deleted shared class library duplicate

--- Shared /obj/Debug/net7.0/Shared.AssemblyInfo.cs
+++ Shared /obj/Debug/net7.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fdc3d18e8aa5ebf0901b93c2aec7f34ecb03dc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
